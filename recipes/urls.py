5fa81417ee5abe330a0001f6eaad112f32a612cf
Sukuna <misaki.usui.otome@gmail.com>
2024-01-25 20:45:21 -0300

    Criação de models e processo de migração

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-
+# recipes:recipe
+app_name = 'recipes'
 
 urlpatterns = [
-    path('' , views.home), # home
-    path('recipes/<int:id>/' , views.recipe), 
+    path('' , views.home, name= "home"), # home
+    path('recipes/<int:id>/' , views.recipe, name= "recipe"), 
     
     
 ]
