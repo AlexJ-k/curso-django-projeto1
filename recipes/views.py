50f55a26a2d95be73c270d82047446d214ec59ea
Sukuna <misaki.usui.otome@gmail.com>
2024-01-26 16:30:16 -0300

    Seção8-tudo-ok

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 ,get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
-from django.http import Http404
+
+
 
 def home (request):
     recipes = Recipe.objects.filter(
@@ -12,21 +13,23 @@ def home (request):
          })
 
 def category (request, category_id):
-    recipes = Recipe.objects.filter(
+   
+   recipes = get_list_or_404(
+       Recipe.objects.filter(
         category__id=category_id,
         is_published=True
-        ).order_by('-id')
-    
-    if not recipes:
-        raise Http404 ('Not found')
-
-    return render (request, 'recipes/pages/category.html', context={
+        ).order_by('-id'))
+   
+   return render (request, 'recipes/pages/category.html', context={
          'recipes': recipes,
-         'title': f'{recipes.first().category.name} - Category |'
+         'title': f'{recipes[0].category.name} - Category |'
          })
 
 def recipe (request,id):
-    return render (request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+
+     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+     
+     return render (request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
         'is_detail_page':True,
     })
