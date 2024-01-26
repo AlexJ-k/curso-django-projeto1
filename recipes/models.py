ada15f37567539027952b92f95f304250878c33d
Sukuna <misaki.usui.otome@gmail.com>
2024-01-26 03:11:22 -0300

    Projeto funcionando - aula 62

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name= models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title= models.CharField(max_length=65)
     description= models.CharField(max_length=165)
@@ -19,13 +22,15 @@ class Recipe(models.Model):
     is_published= models.BooleanField(default=False)
     cover= models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category= models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
     )
     author= models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
 
-
+    def __str__(self):
+        return self.title
 
 
 
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from recipes import views
 
 # recipes:recipe
 app_name = 'recipes'
@@ -7,6 +7,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('' , views.home, name= "home"), # home
     path('recipes/<int:id>/' , views.recipe, name= "recipe"), 
+    path('recipes/category/<int:category_id>/' , views.category, name= "category"),
     
     
 ]
